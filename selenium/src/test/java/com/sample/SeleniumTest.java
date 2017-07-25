c192f84760185a264870f6cf3efffea4e2bc4dc5
KonishiYuji <konishiyuji@KonishiYuji-no-MacBook-Air.local>
2017-07-25 19:38:22 +0900

    init

---
+++ selenium/src/test/java/com/sample/SeleniumTest.java
@@ -0,0 +1,48 @@
+package com.sample;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+// PATH=$PATH:$(pwd)/bin
+// java -jar lib/selenium-server-standalone-3.4.0.jar
+// mvn install -Dmaven.test.skip=true
+public class SeleniumTest {
+	private WebDriver driver;
+
+	private WebDriver getWebDriver() {
+		// return new FirefoxDriver();
+		WebDriver driver = null;
+		DesiredCapabilities firefox = DesiredCapabilities.firefox();
+		try {
+			driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), firefox);
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return driver;
+	}
+
+	@Before
+	public void beforeTest() {
+		driver = getWebDriver();
+		System.setProperty("webdriver.gecko.driver", "/Users/konishiyuji/workspaces/selenium/bin");
+	}
+
+	@Test
+	public void open() {
+		driver.get("https://search.maven.org/");
+	}
+
+	@After
+	public void afterTest() {
+		driver.quit();
+	}
+
+}
